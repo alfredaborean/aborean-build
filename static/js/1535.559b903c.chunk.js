5a09c9e4b214262e1816c73974d20bc1e5b9dcf3
alfredaborean <batashirm@gmail.com>
2025-09-09 17:42:21 +0600

    fsdx

---
+++ static/js/1535.559b903c.chunk.js
@@ -0,0 +1,22 @@
+"use strict";(self.webpackChunk_uniswap_interface=self.webpackChunk_uniswap_interface||[]).push([[1535],{11535:(e,n,i)=>{i.r(n),i.d(n,{default:()=>j});var t=i(97755),a=i(18612),s=i(59375),r=i(96296),d=i(82663),c=i(63933),l=i(99695),o=i(62563),p=i(82746),x=i(59068),u=i(27754);r.ZP.img`
+  max-width: 510px;
+  width: 100%;
+  padding: 0 75px;
+`;const h=r.ZP.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`,g=(0,r.ZP)(h)`
+  gap: 30px;
+`,f=(0,r.ZP)(h)`
+  flex: 1;
+  justify-content: center;
+  gap: 24px;
+  padding-b: 32px;
+
+  @media screen and (min-width: ${({theme:e})=>e.breakpoint.lg}px) {
+    justify-content: space-between;
+    padding-top: 64px;
+  }
+`;function j({title:e,subtitle:n,actionButton:i}){(0,o.Gv)();const r=(0,s.d)(),j=(0,c.s0)(),m=r?l.T.LargeHeader:l.T.Hero,w=r?l.T.HeadlineMedium:l.T.HeadlineLarge;return(0,t.jsx)(f,{children:(0,t.jsxs)(u.default,{logImpression:!0,page:a.InterfacePageName.NOT_FOUND,children:[(0,t.jsx)(g,{children:(0,t.jsxs)(h,{children:[e??(0,t.jsx)(m,{children:"404"}),n??(0,t.jsx)(w,{color:"neutral2",children:(0,t.jsx)(d.Trans,{i18nKey:"common.pageNotFound"})})]})}),i??(0,t.jsx)(p.Flex,{row:!0,alignSelf:"stretch",children:(0,t.jsx)(x.z,{onPress:()=>j("/"),variant:"branded",children:(0,t.jsx)(d.Trans,{i18nKey:"notFound.oops"})})})]})})}}}]);
+//# sourceMappingURL=1535.559b903c.chunk.js.map
